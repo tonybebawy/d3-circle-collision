8d2756ac07dd789708b536b0bb5bc07d91d2c53e
infobase-phac-aspc <bebawy.tony@cegep-heritage.qc.ca>
2019-07-17 17:46:05 +0000

    added basic functionality and using terset instead of uglify

--- src/circleCollision.js
+++ src/circleCollision.js
@@ -1,3 +1,99 @@
+function distance(pos1, pos2) {
+  let [x1, y1] = pos1;
+  let [x2, y2] = pos2;
+  
+  let dx = x1-x2;
+  let dy = y1-y2;
+  
+  return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+}
+
+function parseTransform(value) {
+  let values = value.substring(10, value.length-1).split(',');
+  return [+values[0], +values[1]];
+}
+
+function groups(circles) {
+  let groups = [];
+  
+  circles.each(function(d) {
+    let c1 = d3.select(this);
+    let pos1 = parseTransform(c1.attr("transform"));
+    
+    circles.each(function(k) {
+      let c2 = d3.select(this);
+      let pos2 = parseTransform(c2.attr("transform"));
+      
+      if (+c1.attr("r") + +c2.attr("r") >= distance(pos1, pos2)) {
+        let addToGroup = false;
+        let groupIndex = 0;
+        
+        groups.forEach(function(el, i) {
+          if (el.includes(c1.node())) {
+            addToGroup = true;
+            groupIndex = i;
+          }
+        });
+        
+        if (addToGroup) {
+          if (!groups[groupIndex].includes(c2.node()))
+            groups[groupIndex].push(c2.node());
+        } else {
+          if (c1.node() != c2.node())
+            groups.push([c1.node(), c2.node()]);
+        }
+      }
+    });
+  });
+  
+  return groups;
+}
+
+
+function groupData(groups) {
+  let newlyBoundData = [];
+  
+  groups.forEach(function(group) {
+    let xSum = 0;
+    let ySum = 0;
+
+    let obj = { "data": [], "coordinates": {"x":0,"y":0}};
+    
+    group.forEach(function(element) {
+      let circle = d3.select(element);
+      obj.data.push(circle.datum());
+      
+      let [x, y] = parseTransform(circle.attr("transform"));
+      xSum += x;
+      ySum += y;
+    });
+    
+    let ax = Math.floor(xSum/group.length);
+    let ay = Math.floor(ySum/group.length);
+    
+    obj.coordinates.x = ax;
+    obj.coordinates.y = ay;
+    
+    newlyBoundData.push(obj);
+  });
+  
+  return newlyBoundData;
+}
+
 export default function(selection) {
-  console.log(selection);
+  let newData = groupData(groups(selection));
+
+  return function(tag) {
+    return d3.select(selection.node().parentNode)
+      .selectAll(tag)
+      .data(newData)
+      .enter()
+      .append(tag)
+      .attr("transform", function(d) {
+        let x = d.coordinates.x;
+        let y = d.coordinates.y;
+  
+        return `translate(${x},${y})`;
+      });
+    }
 }
