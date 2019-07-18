c61b781e2be12f1dd96a9d35176972e3c557fe49
Tony Bebawy <bebawy.tony@cegep-heritage.qc.ca>
2019-07-17 20:59:26 -0400

    added some tests

---
+++ test/circleCollision.test.js
@@ -0,0 +1,17 @@
+const { JSDOM } = require("jsdom");
+const d3Selection = require('d3-selection');
+
+test("svg gets created", () => {
+  let DOM = new JSDOM("<html><body></body></html>");
+
+  const window = DOM.window;
+
+  let svg = d3Selection.select(window.document)
+    .select('body')
+    .append('svg')
+    .attr('width', 500)
+    .attr('height', 500);
+
+  expect(svg.attr('height')).toBe("500");
+  expect(svg.attr('width')).toBe("500");
+});
