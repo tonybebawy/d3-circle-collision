b79ec804d459727a87d371279bbfc3dd05807e3f
Tony Bebawy <bebawy.tony@cegep-heritage.qc.ca>
2019-07-18 20:38:48 -0400

    added some tests boilerplate

--- test/circleCollision.test.js
+++ test/circleCollision.test.js
@@ -3,18 +3,39 @@ const d3Selection = require('d3-selection');
 
 function render() {
     let DOM = new JSDOM("<html><body></body></html>");
-    const window = DOM.window;
+    let window = DOM.window;
+
+    let data = [{
+        "title": "circle1",
+        "coordinates": {
+            "x": 10,
+            "y": 10
+        }
+    }, {
+        "title": "circle2",
+        "coordinates": {
+            "x": 13,
+            "y": 13
+        }
+    }];
 
     return d3Selection.select(window.document)
         .select('body')
         .append('svg')
         .attr('width', 500)
-        .attr('height', 500);
+        .attr('height', 500)
+        .selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr("transdform", function (d) {
+            return `translate(${d.coordinates.x},${d.coordinates.y})`
+        })
+        .attr('r', 10);
 }
 
-test("svg gets created", () => {
+test("circles get created", () => {
     let circles = render();
 
-    expect(circles.attr('height')).toBe("500");
-    expect(circles.attr('width')).toBe("500");
+    expect(circles.nodes().length).toBe(2);
 });
